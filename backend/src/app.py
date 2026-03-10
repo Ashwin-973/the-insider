425f52f50941085be23e0fb4f553166a3075e0f8
Tony Montana <ashwin.s1336@gmail.com>
2026-03-10 06:53:06 +0530

    feat : add GET endpoints

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 
 app=FastAPI()
 
-@app.get("/favorite-movies")
-def favorite_movies():
-    return {"movie1":"Fight Club","movie2":"Leon: The Professional"}
+posts={
+    1:{
+        "title":"Billy Costigan infiltrates the criminal organization of Frank Costello",
+        "year":2006
+    },
+    2:{
+        "title":"330,000 French, British, Belgian and Dutch soldiers were safely evacuated from dunkirk",
+        "year":2017
+    }
+}
+
+
+@app.get("/posts")
+def get_posts():
+    return posts
+
+@app.get("/posts/{id}")
+def get_post_by_id(id:int):
+    if not posts.get(id):
+        raise HTTPException(status_code=404,detail=f"Post with {id} not found")
+    return posts.get(id)
