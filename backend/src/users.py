0784f867bf918f86488d3652a6d43b945ca87a49
Tony Montana <ashwin.s1336@gmail.com>
2026-03-17 20:16:34 +0530

    feat : configure User Model

---
+++ backend/src/users.py
@@ -0,0 +1,46 @@
+import uuid
+import os
+from typing import Optional
+from dotenv import load_dotenv
+
+from fastapi import Depends,Request
+from fastapi_users import BaseUserManager,FastAPIUsers,UUIDIDMixin,models
+from fastapi_users.authentication import (
+    AuthenticationBackend,
+    BearerTransport,
+    JWTStrategy
+)
+from fastapi_users.db import SQLAlchemyUserDatabase
+
+from src.db import User,get_user_db
+
+
+load_dotenv()
+
+
+class UserManager(UUIDIDMixin,BaseUserManager[User,uuid.UUID]):
+    reset_password_token_secret=os.getenv("JWT_SECRET")
+    verification_token_secret=os.getenv("JWT_SECRET")
+
+    async def on_after_register(self, user:User, request:Optional[Request] = None):
+        print(f"User {user.id} has registered")
+
+    async def on_after_forgot_password(self, user:User, token:str, request:Optional[Request] = None):
+        print(f"User {user.id} has forgot their password . Reset token : {token}")
+    
+    async def on_after_request_verify(self, user:User, token:str, request:Optional[Request] = None):
+        print(f"Verification requested for user {user.id} . Verification token : {token}")
+    
+
+async def get_user_manager(user_db:SQLAlchemyUserDatabase=Depends(get_user_db)):
+    yield UserManager(user_db)
+
+
+    
+
+
+
+
+
+
+
