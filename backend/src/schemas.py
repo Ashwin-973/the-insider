dc39c0af99b306bdd60aaedc3c6b142c9b1224ab
Tony Montana <ashwin.s1336@gmail.com>
2026-03-17 22:38:10 +0530

    chore : add new schema and change project settings

--- backend/src/schemas.py
+++ backend/src/schemas.py
@@ -1,6 +1,18 @@
 from pydantic import BaseModel
 
+from fastapi_users import schemas
+import uuid
+
 
 class PostFormat(BaseModel):
     title:str
     content:str
+
+class UserRead(schemas.BaseUser[uuid.UUID]):
+    pass
+
+class UserCreate(schemas.BaseUserCreate):
+    pass
+
+class UserUpdate(schemas.BaseUserUpdate):
+    pass
